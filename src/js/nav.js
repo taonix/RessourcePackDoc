f8f353a57ec335b6916071fc94786dc95c71979d
taonix <taonix314@gmail.com>
2021-01-18 20:10:24 +0100

    Initial commit

---
+++ src/js/nav.js
@@ -0,0 +1,85 @@
+$(function() {
+    $("body").prepend("<nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">\n" +
+        "        <div class=\"container-fluid\">\n" +
+        "            <a class=\"navbar-brand\" href=\"#\">RessourcePackDoc</a>\n" +
+        "            <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
+        "                <span class=\"navbar-toggler-icon\"></span>\n" +
+        "            </button>\n" +
+        "            <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\n" +
+        "                <ul class=\"navbar-nav me-auto mb-2 mb-lg-0\">\n" +
+        "                    <li class=\"nav-item dropdown\">\n" +
+        "                        <a class=\"nav-link\" href=\"./src/menus/preambule.html\" id=\"Main\" role=\"button\" aria-expanded=\"false\">\n" +
+        "                            Préambule\n" +
+        "                        </a>\n" +
+        "                    </li>\n" +
+        "                    <li class=\"nav-item dropdown\">\n" +
+        "                        <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"Texture\" role=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">\n" +
+        "                            Texture\n" +
+        "                        </a>\n" +
+        "                        <ul class=\"dropdown-menu dropdown-menu-dark\" aria-labelledby=\"navbarDropdown\">\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Presentation</a></li>\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Another action</a></li>\n" +
+        "                            <li><hr class=\"dropdown-divider\"></li>\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Organisation des fichiers</a></li>\n" +
+        "                        </ul>\n" +
+        "                    </li>\n" +
+        "                    <li class=\"nav-item dropdown\">\n" +
+        "                        <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"Son\" role=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">\n" +
+        "                            Son\n" +
+        "                        </a>\n" +
+        "                        <ul class=\"dropdown-menu dropdown-menu-dark\" aria-labelledby=\"navbarDropdown\">\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Presentation</a></li>\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Ajouter des sons customs</a></li>\n" +
+        "                            <li><hr class=\"dropdown-divider\"></li>\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Organisation des fichiers</a></li>\n" +
+        "                        </ul>\n" +
+        "                    </li>\n" +
+        "                    <li class=\"nav-item dropdown\">\n" +
+        "                        <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"3D\" role=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">\n" +
+        "                            3D\n" +
+        "                        </a>\n" +
+        "                        <ul class=\"dropdown-menu dropdown-menu-dark\" aria-labelledby=\"navbarDropdown\">\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Presentation</a></li>\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Another action</a></li>\n" +
+        "                            <li><hr class=\"dropdown-divider\"></li>\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Organisation des fichiers</a></li>\n" +
+        "                        </ul>\n" +
+        "                    </li>\n" +
+        "                    <li class=\"nav-item dropdown\">\n" +
+        "                        <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"Shadering\" role=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">\n" +
+        "                            Shadering\n" +
+        "                        </a>\n" +
+        "                        <ul class=\"dropdown-menu dropdown-menu-dark\" aria-labelledby=\"navbarDropdown\">\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Presentation</a></li>\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Another action</a></li>\n" +
+        "                            <li><hr class=\"dropdown-divider\"></li>\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Organisation des fichiers</a></li>\n" +
+        "                        </ul>\n" +
+        "                    </li>\n" +
+        "                    <li class=\"nav-item dropdown\">\n" +
+        "                        <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"Particules\" role=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">\n" +
+        "                            Particules\n" +
+        "                        </a>\n" +
+        "                        <ul class=\"dropdown-menu dropdown-menu-dark\" aria-labelledby=\"navbarDropdown\">\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Presentation</a></li>\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Another action</a></li>\n" +
+        "                            <li><hr class=\"dropdown-divider\"></li>\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Organisation des fichiers</a></li>\n" +
+        "                        </ul>\n" +
+        "                    </li>\n" +
+        "                    <li class=\"nav-item dropdown\">\n" +
+        "                        <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"GuiText\" role=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">\n" +
+        "                            GUI + Texts\n" +
+        "                        </a>\n" +
+        "                        <ul class=\"dropdown-menu dropdown-menu-dark\" aria-labelledby=\"navbarDropdown\">\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Presentation</a></li>\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Another action</a></li>\n" +
+        "                            <li><hr class=\"dropdown-divider\"></li>\n" +
+        "                            <li><a class=\"dropdown-item\" href=\"#\">Organisation des fichiers</a></li>\n" +
+        "                        </ul>\n" +
+        "                    </li>\n" +
+        "                </ul>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </nav>");
+});
